48b02cb81fcd5b76953fde569e5598036c9e169c
Danny <dantes817@gmail.com>
2020-04-14 20:48:46 +0200

    added logic for clock proj. into app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const clockus = () => {
+  const allDate = new Date();
+
+  let hour = allDate.getHours(),
+    min = allDate.getMinutes(),
+    sec = allDate.getSeconds();
+
+  hour < 10 ? (hour = `{0} hour`) : hour;
+  min < 10 ? (min = `{0} min`) : min;
+  sec < 10 ? (sec = `{0} sec`) : sec;
+
+  document.getElementById("hours").innerHTML = `${hour}`;
+  document.getElementById("minutes").innerHTML = `: ${min}`;
+  document.getElementById("seconds").innerHTML = `: ${sec}`;
+  return clockus;
+};
+
+setInterval(clockus(), 100);
